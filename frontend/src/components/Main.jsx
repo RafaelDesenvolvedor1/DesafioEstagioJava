a42c72d9c7e1a9ca1333f768980634f08285fa65
Rafael Santos <rafaelcachorroquentefoda@gmail.com>
2025-05-04 18:48:10 -0300

    refactory Main component

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 
 import { ListContext } from "../contexts/list";
 import { Task } from "./Task";
@@ -8,70 +8,32 @@ import { MdOutlineAddCircle } from "react-icons/md";
 import Modal from "./Modal";
 
 export function Main() {
-  const { lista } = useContext(ListContext);
+  const { list } = useContext(ListContext);
 
   return (
     <div
       id="containerApp"
       className="rounded-t-4xl bg-white mt-[-30px]  min-h-[60dvh] py-7 flex flex-wrap w-full px-7 gap-7 max-sm:justify-center"
     >
-     
-        <button
-          type="button"
-          className="py-2.5 px-5 mt-7 font-medium bg-gray-100 text-primary focus:outline-none  rounded-lg border border-gray-200 hover:bg-gray-200 hover:text-primary-200 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 w-sm h-[300px] flex justify-center items-center text-8xl cursor-pointer"
-          data-modal-target="authentication-modal"
-          data-modal-toggle="authentication-modal"
-         
-        >
-          <MdOutlineAddCircle />
-        </button>
-        {lista.map((item) => (
-          <Task
-            key={item.id}
-            id={item.id}
-            titulo={item.titulo}
-            text={item.descricao}
-            checked={item.concluido}
-          />
-        ))}
-      
-       <Modal />
-       </div>
+      <button
+        type="button"
+        className="py-2.5 px-5 mt-7 font-medium bg-gray-100 text-primary focus:outline-none  rounded-lg border border-gray-200 hover:bg-gray-200 hover:text-primary-200 focus:z-10 focus:ring-4 focus:ring-gray-100 w-sm h-[300px] flex justify-center items-center text-8xl cursor-pointer"
+        data-modal-target="authentication-modal"
+        data-modal-toggle="authentication-modal"
+      >
+        <MdOutlineAddCircle />
+      </button>
+      {list.map((item) => (
+        <Task
+          key={item.id}
+          id={item.id}
+          titulo={item.titulo}
+          text={item.descricao}
+          checked={item.concluido}
+        />
+      ))}
+
+      <Modal />
+    </div>
   );
 }
-
-// <form onSubmit={handleAddTask}>
-// <input
-//   type="text"
-//   placeholder="Titulo"
-//   value={inputTitle}
-//   onChange={(e) => setInputTitle(e.target.value)}
-//   required
-// />
-
-// <input
-//   type="text"
-//   placeholder="Text"
-//   value={inputText}
-//   onChange={(e) => setInputText(e.target.value)}
-//   required
-// />
-
-// <button>Add task</button>
-// </form>
-
-// <div>
-// {taskConcludes} / {lista.length}
-// </div>
-
-// <ul>
-// {lista.map((item) => (
-//   <Task
-//     key={item.id}
-//     id={item.id}
-//     titulo={item.titulo}
-//     text={item.descricao}
-//     checked={item.concluido}
-//   />
-// ))}
-// </ul>
